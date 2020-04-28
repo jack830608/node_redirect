ac93473faf4ab23d967c363e7a23d715f1b29aaf
JackLin <jack830608@gmail.com>
2020-04-28 17:13:23 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const path = require('path');
 
 
-
-const port = process.env.PORT || 6000
-app.get('/', (req, res) => {
-    res.status(301).redirect("https://www.jack1in.blog")
+app.use(morgan('dev'));
+const port = process.env.PORT || 7000
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',(req,res) => {
+    res.redirect(301,'https://www.jack1in.blog')
 })
 app.listen(port, (err) => {
     if (err) throw err
